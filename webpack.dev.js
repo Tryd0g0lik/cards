30566b243e1761155405aede94448a24bee9d766
Tryd0g0lik <work80@mail.ru>
2024-01-13 15:13:32 +0700

    [master]: the zero build

---
+++ webpack.dev.js
@@ -0,0 +1,29 @@
+const path = require("path");
+const { merge } = require('webpack-merge');
+const common = require('./webpack.config');
+module.exports = merge(common, {
+  watchOptions: {
+    ignored: [
+      "**/backend",
+      "dist\\db",
+      "dist\\server",
+      "node_modules",
+      "**/node_modules"
+    ]
+  },
+  devServer: {
+    static: {
+      directory: path.resolve(__dirname, './src/index.ts'),
+
+    },
+
+    watchFiles: [
+      'src/fronted/src'
+    ],
+
+    compress: true,
+    historyApiFallback: true,
+    open: true,
+    // port: 8080
+  },
+})
